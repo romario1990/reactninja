99821f77f115bb292f88eb354e1516d5d89062a8
romario_vargas <romario.geomais@gmail.com>
2020-04-18 10:50:33 -0300

    Finalizado configuração inicial do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
 "use strict";
 
 const path = require("path");
+const webpack = require("webpack");
+const validate = require("webpack-validator");
 
-module.exports = {
+module.exports = validate({
   devtool: "source-map",
 
-  entry: path.join(__dirname, "src", "index"),
+  entry: [
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?http://localhost:3000",
+    "webpack/hot/only-dev-server",
+    path.join(__dirname, "src", "index"),
+  ],
 
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
     publicPath: "/static/",
   },
+
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 
   module: {
     loaders: [
@@ -23,4 +32,4 @@ module.exports = {
       },
     ],
   },
-};
+});
